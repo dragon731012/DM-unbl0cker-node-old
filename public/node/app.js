25163064a447e103cd1deb46d0d1a1dfd7b13a4b
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-02-28 14:57:12 -0600

    Update app.js

--- public/node/app.js
+++ public/node/app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/p/',
+    prefix: '/node/p/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
