2684a557d31b251a69691e02e077e4ade8e3f48a
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-02-28 12:59:23 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -26,11 +26,37 @@ function getProxy(url = input.value.trim()) {
         window.location.href =  `/rhodium/gateway?url=${url}`;
     } 
     if (localStorage.getItem('proxy') === 'node') {
-        if (url.includes(".")==false){
-            url="https://www.google.com/search?q="+url;
-        }
-        if (!isUrlVal(url)) url = 'http://' + url;
-        window.location.href =  `p/${url}`;
+        		function $(id){
+			return document.getElementById(id);
+		}
+		
+		$('unblocker-form').onsubmit = function(){
+			var url = $('url').value;
+			if(url.substr(0,4) != "http"){
+				url = "http://" + url;
+			}
+			window.location.pathname = 'p/' + url;
+			return false;
+		};
+		
+		function checkError(){
+			var search = window.location.search;
+			var start = search.indexOf('error=');
+			if(start > -1){
+				var stop = search.indexOf('&', start);
+				if(stop == -1){ stop = undefined; }
+				// +6 for "error="
+				var err = search.substr(start+6, stop);
+                var $error = $('error');
+				$error.innerText = $error.textContent = decodeURIComponent(err);
+				$error.style.display = "block";
+			}
+		}
+    
+		window.onload = function() { 
+			$('url').focus(); 
+			checkError(); 
+		}
     } 
 };
 function setProxy(proxy) {
